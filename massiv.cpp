b193989d0796e211d4618b6aeaba55b4adcb3c31
DanilBukreev <31454061+DanilBukreev@users.noreply.github.com>
2017-10-22 14:20:02 +0300

    Update massiv.cpp

--- massiv.cpp
+++ massiv.cpp
@@ -1,68 +1,72 @@
 #include <iostream>
 using namespace std;
 
-void Merg(int arr[],int begin, int end) {
- int i = begin;
- int mid = begin + (end - begin)/2;
- int j = mid + 1;
- int k = 0;
- int d[5];     // массив куда будем складывать элементы после слияния
- while (i<= mid && j<= end){
-        if (arr[i] <= arr[j]){
-            d[k] = arr[i]; i++;
-        }
-        else {
-             d[k] = arr[j]; j++;
-        }
-        k++;
- }
- while (i<=mid){
-        d[k] = arr[i];
-        i++; k++;
- }
- while (j <= end){
-        d[k] = arr[j];
-        j++;k++;
- }
+void Merg(int arr[], int begin, int end) {
+  int i = begin;
+  int mid = begin + (end - begin) / 2;
+  int j = mid + 1;
+  int k = 0;
+  int d[5];  // массив куда будем складывать элементы после слияния
+  while (i <= mid && j <= end) {
+    if (arr[i] <= arr[j]) {
+      d[k] = arr[i];
+      i++;
+    } else {
+      d[k] = arr[j];
+      j++;
+    }
+    k++;
+  }
+  while (i <= mid) {
+    d[k] = arr[i];
+    i++;
+    k++;
+  }
+  while (j <= end) {
+    d[k] = arr[j];
+    j++;
+    k++;
+  }
 }
-void MergSort (int * arr, int left, int right){
- int temp = 0;
- if (left < right )
-     if (right - left == 1) {
-        if (arr[left] > arr[right]){
-            temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-        }
+void MergSort(int *arr, int left, int right) {
+  int temp = 0;
+  if (left < right)
+    if (right - left == 1) {
+      if (arr[left] > arr[right]) {
+        temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
       }
-      else {
-           MergSort(arr,left,left+(right - left)/2);
-           MergSort(arr,left+(right- left)/2,right);
-           Merg(arr,left,right);
-      }
+    } else {
+      MergSort(arr, left, left + (right - left) / 2);
+      MergSort(arr, left + (right - left) / 2, right);
+      Merg(arr, left, right);
+    }
 }
- void input(int *m,int &n){
-cout << "Введите количество элементов массива ";
+void input(int *m, int &n) {
+  cout << "Введите количество элементов массива ";
   cin >> n;
-  for (int i = 0; i < n; i++){
-       cout << "a[" << i << "]=";
-       cin >> m[i];
+  for (int i = 0; i < n; i++) {
+    cout << "a[" << i << "]=";
+    cin >> m[i];
   }
 }
- void print(int*m,int n){
-  for (int i = 0; i<n;i++){
-      cout << m[i] << " ";
-      cout << endl;
+void print(int *m, int n) {
+  for (int i = 0; i < n; i++) {
+    cout << m[i] << " ";
+    cout << endl;
   }
- }
- int main() {
+}
+int main() {
   int n, arr[5];
-  input(arr,n);
+  input(arr, n);
   cout << " Исходный массив " << endl;
-  print (arr,n);
+  print(arr, n);
 
-  MergSort (arr,0,n-1);
+  MergSort(arr, 0, n - 1);
 
   cout << " Отсортированный массив " << endl;
-  print (arr,n);
-}
+  print(arr, n);
+} 
+     
+   
